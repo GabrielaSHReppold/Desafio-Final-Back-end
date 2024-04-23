97bfbb9a39d8afa189cbbb4d881536e7e4312805
Gabriela Reppold <reppoldgabriela@gmail.com>
2024-04-23 10:31:32 -0300

    primeiro commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3333,()=> console.log("Servidor rodando na porta 3333"))
